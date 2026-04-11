2c61f38f6681f49e3f66e6228b02f556fbfc5297
Jonas Kleinebecker <jonaskleinebecker@gmail.com>
2026-04-11 12:32:49 +0200

    merge changes from desktop

--- env/.config/nvim/lua/plugins/lualine.lua
+++ env/.config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,14 @@
-return{
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts={}
+local lint_progress = function()
+	local linters = require("lint").get_running()
+	if #linters == 0 then
+		return "󰦕"
+	end
+	return "󱉶 " .. table.concat(linters, ", ")
+end
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		sections = { lualine_y = { lint_progress, "progress" } },
+	},
 }
