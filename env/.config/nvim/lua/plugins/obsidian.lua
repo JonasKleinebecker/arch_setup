1b0302c1e4212c50d0d2125fb937b9d1a272d00d
Jonas Kleinebecker <jonaskleinebecker@gmail.com>
2026-04-15 00:48:58 +0200

    Add note scripts and obsidian opts

--- env/.config/nvim/lua/plugins/obsidian.lua
+++ env/.config/nvim/lua/plugins/obsidian.lua
@@ -4,6 +4,33 @@ return {
 	---@module 'obsidian'
 	---@type obsidian.config
 	opts = {
+		callbacks = {
+			enter_note = function(note)
+				vim.keymap.set("n", "<leader>tc", "<cmd>Obsidian toggle_checkbox<cr>", {
+					buffer = true,
+					desc = "Toggle checkbox",
+				})
+			end,
+		},
+		checkbox = {
+			order = { " ", "x" },
+		},
+		-- Optional, customize how note IDs are generated given an optional title.
+		---@param title string|?
+		---@return string
+		note_id_func = function(title)
+			local suffix = ""
+			if title ~= nil then
+				-- If title is given, transform it into valid file name.
+				suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+			else
+				-- If title is nil, just add 4 random uppercase letters to the suffix.
+				for _ = 1, 4 do
+					suffix = suffix .. string.char(math.random(65, 90))
+				end
+			end
+			return suffix .. ".md"
+		end,
 		legacy_commands = false, -- this will be removed in the next major release
 		workspaces = {
 			{
